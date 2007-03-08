d60302112a680ac41f86f74c78e0d64008a09609
Riccardo Mottola <rm@gnu.org>
2007-03-08 15:49:31 +0000

    updated copyright and license
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/simplewebkit/trunk@24824 72102866-910b-0410-8b05-ffd578937521

--- Sources/DOMCore.h
+++ Sources/DOMCore.h
@@ -1,10 +1,27 @@
-//
-//  DOMCore.h
-//  SimpleWebKit
-//
-//  Created by Dr. H. Nikolaus Schaller on 02.11.06.
-//  Copyright 2006 __MyCompanyName__. All rights reserved.
-//
+/* simplewebkit
+   DOMCore.h
+
+   Copyright (C) 2007 Free Software Foundation, Inc.
+
+   Author: Dr. H. Nikolaus Schaller
+
+   This file is part of the GNUstep Database Library.
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public
+   License along with this library; see the file COPYING.LIB.
+   If not, write to the Free Software Foundation,
+   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*/
 
 #ifdef __mySTEP__
 // gcc 2.95.3 does not allow methods with the same name as a class, i.e.
