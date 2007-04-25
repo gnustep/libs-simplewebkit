b85361ed9135d542ed0d990d636069abf173dfc0
Riccardo Mottola <rm@gnu.org>
2007-04-25 09:44:36 +0000

    restored header
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/simplewebkit/trunk@25074 72102866-910b-0410-8b05-ffd578937521

--- Sources/DOM.h
+++ Sources/DOM.h
@@ -1,10 +1,25 @@
-//
-//  DOM.h
-//  SimpleWebKit
-//
-//  Created by Dr. H. Nikolaus Schaller on 02.11.06.
-//  Copyright 2006 __MyCompanyName__. All rights reserved.
-//
+/* simplewebkit
+   DOM.h
+
+   Copyright (C) 2006 Free Software Foundation, Inc.
+
+   Author: Dr. H. Nikolaus Schaller
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public
+   License along with this library; see the file COPYING.LIB.
+   If not, write to the Free Software Foundation,
+   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*/
 
 #import <Cocoa/Cocoa.h>
 
