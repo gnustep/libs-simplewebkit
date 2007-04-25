781fb186e36ff9953710938fff3b7f6d18f0e483
Riccardo Mottola <rm@gnu.org>
2007-04-25 10:13:51 +0000

    restored header
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/simplewebkit/trunk@25079 72102866-910b-0410-8b05-ffd578937521

--- Sources/DOMHTML.m
+++ Sources/DOMHTML.m
@@ -1,11 +1,25 @@
-//
-//  DOMHTML.m
-//  SimpleWebKit
-//
-//  Created by Nikolaus Schaller on 28.01.07.
-//  Copyright 2007 __MyCompanyName__. All rights reserved.
-//
+/* simplewebkit
+   DOMHTML.m
 
+   Copyright (C) 2007 Free Software Foundation, Inc.
+
+   Author: Dr. H. Nikolaus Schaller
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public
+   License along with this library; see the file COPYING.LIB.
+   If not, write to the Free Software Foundation,
+   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*/
 // FIXME: learn from http://www.w3.org/TR/2004/REC-DOM-Level-3-Core-20040407/core.html
 // FIXME: add additional attributes (e.g. images, anchors etc. for DOMHTMLDocument) and DOMHTMLCollection type
 // FIXME: we should separate code from DOM Tree management, HTML parsing, and visual representation
